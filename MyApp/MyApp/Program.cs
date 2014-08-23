481837154c224a4daf285ab162472ce87e1aa767
Helio Dutra <HJDUTRA@TPG.COM.AU>
2014-08-23 10:59:35 +1000

    Test 03

--- MyApp/MyApp/Program.cs
+++ MyApp/MyApp/Program.cs
@@ -10,7 +10,7 @@ namespace MyApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Test 02");
+            Console.WriteLine("Test 03");
         }
     }
 }
